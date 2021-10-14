152f38af5371803d4e25cb4490bb4b373b1b0f68
Greedcold <alonezuko@mail.ru>
2021-10-14 22:57:14 +0700

    create func create article like in rails console

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,25 @@
 class ArticlesController < ApplicationController
 
-#attr accessors?
-def index
-  @articles = Article.all
-end
+  #attr accessors?
+  def index
+    @articles = Article.all
+  end
 
-def show
-  @article = Article.find(params[:id])
-end
+  def show
+    @article = Article.find(params[:id])
+  end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #зачем повтор метода new?
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.save
+
+    redirect_to article_path(@article)
+  end
 end
