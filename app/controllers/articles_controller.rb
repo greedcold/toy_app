cee586020d15fde4d2eed5501c30e2677f159138
Greedcold <alonezuko@mail.ru>
2021-10-20 19:50:19 +0700

    create article function with strong parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,12 +14,13 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #зачем повтор метода new?
-    @article = Article.new
-    @article.title = params[:article][:title]
-    @article.body = params[:article][:body]
+    @article = Article.new(article_params)
     @article.save
 
     redirect_to article_path(@article)
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
